543f0e7ed953a39844e14c4da0b9e6657a7b44c5
jiubanyipeng <2956098898@qq.com>
2023-06-18 19:03:33 +0800

    更新

--- 1.py
+++ 1.py
@@ -1,24 +1,15 @@
-import json
+import requests
+from lxml import etree
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+}
 
-# 提取分类中的关键字
-def extract_keywords(data, keyword_list):
-    if isinstance(data, dict):
-        for value in data.values():
-            if isinstance(value, str):
-                keyword_list.append(value)
-            elif isinstance(value, (list, dict)):
-                extract_keywords(value, keyword_list)
-    elif isinstance(data, list):
-        for item in data:
-            if isinstance(item, str):
-                keyword_list.append(item)
-            elif isinstance(item, (list, dict)):
-                extract_keywords(item, keyword_list)
+s = 2016
+url = f'https://www.techpowerup.com/gpu-specs/?released={s}&sort=name'
+xpaths = '//*[@class="vendor-ATI"]/a/text()'
 
-
-with open('./分类.json','r',encoding='utf-8') as f:
-    data = json.load(f)
-    keywords = []
-    extract_keywords(data, keywords)
-    print(keywords)
+response = requests.get(url, headers=headers)
+html = etree.HTML(response.text)
+gpu_names = html.xpath(xpaths)
+print(gpu_names)
--- test.py
+++ test.py
@@ -1,145 +1,30 @@
 import json
 
 
-def get_strings(data):
-    if type(data) == list:
-        return {'mes':False,'data':data,'type':list}
-    if type(data) == dict:
-        return {'mes':False,'data':data,'type':dict}
-    else:
-        return {'mes': True, 'data': data,}
+# 提取分类中的关键字
+def extract_keywords(data, keyword_list):
+    if isinstance(data, dict):
+        for value in data.values():
+            if isinstance(value, str):
+                keyword_list.append(value)
+            elif isinstance(value, (list, dict)):
+                extract_keywords(value, keyword_list)
+    elif isinstance(data, list):
+        for item in data:
+            if isinstance(item, str):
+                keyword_list.append(item)
+            elif isinstance(item, (list, dict)):
+                extract_keywords(item, keyword_list)
 
 
-with open('./分类.json','r',encoding='utf-8') as f:
-    data = json.load(f)
-    for key,value in data.items():
-        print(f'{key}:{value}')
-    # for value in s.values():
-    #     if type(value) == list:
-    #         for value1 in value:
-    #             if type(value1) == list:
-    #                 for value2 in value1:
-    #                     if type(value2) == list:
-    #                         for value3 in value2:
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.value():
-    #                                     print(value4)
-    #                             else:
-    #                                 print(value3)
-    #                     elif type(value2) == dict:
-    #                         for value3 in value2.value():
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.value():
-    #                                     print(value4)
-    #                             else:
-    #                                 print(value3)
-    #                     else:
-    #                         print(value2)
-    #             elif type(value1) == dict:
-    #                 for value2 in value1.values():
-    #                     if type(value2) == list:
-    #                         for value3 in value2:
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.values():
-    #                                     print(value4)
-    #                             else:
-    #                                 print(value3)
-    #                     elif type(value2) == dict:
-    #                         for value3 in value2.value():
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.values():
-    #                                     print(value4)
-    #                             else:
-    #                                 print(value3)
-    #                     else:
-    #                         print(value2)
-    #             else:
-    #                 print(value1)
-    #     elif type(value) == dict:
-    #         for value1 in value.values():
-    #             if type(value1) == list:
-    #                 for value2 in value1:
-    #                     if type(value2) == list:
-    #                         for value3 in value2:
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.values():
-    #                                     print(value4)
-    #                             else:
-    #                                 print(value3)
-    #                     elif type(value2) == dict:
-    #                         for value3 in value2.values():
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.values():
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             else:
-    #                                 print(value3)
-    #                     else:
-    #                         print(value2)
-    #             elif type(value1) == dict:
-    #                 for value2 in value1.values():
-    #                     if type(value2) == list:
-    #                         for value3 in value2:
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.values():
-    #                                     print(value4)
-    #                             else:
-    #                                 print(value3)
-    #                     elif type(value2) == dict:
-    #                         for value3 in value2.values():
-    #                             if type(value3) == list:
-    #                                 for value4 in value3:
-    #                                     if type(value4) == list:
-    #                                         for value5 in value4:
-    #                                             print(5)
-    #                             elif type(value3) == dict:
-    #                                 for value4 in value3.values():
-    #                                     print(value4)
-    #                             else:
-    #                                 print(value3)
-    #                     else:
-    #                         print(value2)
-    #             else:
-    #                 print(value1)
-    #     else:
-    #         print(value)
+# 返回提取分类中的关键字
+def keywords():
+    with open('./分类.json','r',encoding='utf-8') as f:
+        data = json.load(f)
+        keywords = []
+        extract_keywords(data, keywords)
+        return keywords
 
 
 
-
+print(keywords())
